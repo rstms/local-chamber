41ddcb0ee3cb991433a4f63f9c3b3fd254cc4bb9
Matt Krueger <mkrueger@rstms.net>
2022-10-18 08:30:24 -0500

    Bump version: 1.6.1 → 1.7.0

--- local_chamber/version.py
+++ local_chamber/version.py
@@ -1,4 +1,4 @@
-__version__ = "1.6.1"
+__version__ = "1.7.0"
 __author__ = "Matt Krueger"
 __email__ = "mkrueger@rstms.net"
-__timestamp__ = "2022-08-26 05:28:47-05:00"
+__timestamp__ = "2022-10-18 08:30:24-05:00"
